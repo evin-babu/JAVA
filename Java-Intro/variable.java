cab60ec662ca4cf39dd64840cf964be28ef57780
evin-babu <evinbabu.official@gmail.com>
2025-06-21 10:56:58 +0530

    input & Variables

---
+++ Java-Intro/variable.java
@@ -0,0 +1,15 @@
+public class variable {
+    public static void main(String args[]){
+        String name ="Evin";
+        int age=22;
+        double Salary=5000.111;
+        int a=1;
+        int b=2;
+        int sum =a+b;
+        System.out.println(sum);
+        System.out.println(age);
+        System.out.println(Salary);
+        System.out.println(name);
+
+    }
+}
