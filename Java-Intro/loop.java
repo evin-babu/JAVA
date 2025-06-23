8761d0f00cd3e1f33924655c29db4a5b507ff8a3
evin-babu <evinbabu.official@gmail.com>
2025-06-23 09:59:56 +0530

    loops

---
+++ Java-Intro/loop.java
@@ -0,0 +1,15 @@
+public class loop {
+    public static void  main(String[] args){
+        // for(int c=1;c<=10;c++){
+        //     System.out.println(c);
+        // }
+        int i=0;
+        // while(i<11){
+        //     System.out.println(i);
+        //     i=i+1;
+        do{System.out.println(i);
+        i=i+1;}
+        while(i<11);
+        
+    }
+}
